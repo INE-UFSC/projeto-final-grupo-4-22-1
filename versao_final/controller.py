6aabf87ae2279f2391202ef53180e5532fd6c0b8
Pedro <pedro.nackm@gmail.com>
2022-07-08 21:47:19 -0300

    Criacao da classe Mapa

--- versao_final/controller.py
+++ versao_final/controller.py
@@ -5,6 +5,7 @@ from rankingDAO import RankingDAO
 import pygame
 from pygame.locals import *
 from inimigos import Inimigo
+from colisoes import Colisoes
 
 from terreno.aquatico import Aquatico
 from sapo import Sapo
@@ -21,6 +22,7 @@ from itens.espinho import Espinho
 from itens.cogumelo import Cogumelo
 
 from coordenada import Coordenada
+from mapa import Mapa
 #c_flor = Coordenada(0,0,0)
 #c_item = Coordenada(0,0,0)
 c1 = Coordenada(0,0,0)
@@ -33,35 +35,27 @@ class GameController:
     def __init__(self):
         self.__rankingDAO = RankingDAO()
         self.__ranking = Ranking(self.__rankingDAO)
+        self.__colisoes = Colisoes()
+        self.__mapa = Mapa()
 
         self.__tela = TelaJogo(self)
         self.__clock = pygame.time.Clock()
         
-        self.__lista_player = pygame.sprite.Group()
         self.__jogador = None
 
-        self.__lista_parceiro = pygame.sprite.Group()
         self.__colisoes_parceiro = None
 
-        self.__lista_cobras = pygame.sprite.Group()
         self.__colisoes_cobra = None
 
-        self.__lista_jacares = pygame.sprite.Group()
         self.__colisoes_jacare = None
 
-        self.__lista_flores = pygame.sprite.Group()
         self.__colisoes_flores = None
 
-        self.__lista_consumiveis = pygame.sprite.Group()
         self.__colisoes_consumiveis = None
 
-        self.__lista_cogumelos = pygame.sprite.Group()
         self.__colisoes_cogumelos = None
 
-        self.__lista_terreno_aquatico = pygame.sprite.Group()
         self.__colisoes_terreno_aquatico = None
-        
-        self.__all_sprites = pygame.sprite.Group()
         
         #chave é o objeto flor e o valor é bool p/ saber se seu peso foi ou não descontado da velocidade do sapo
         self.__flores_coletadas = {}
@@ -109,65 +103,28 @@ class GameController:
 
     def iniciar(self):
         self.__jogador = Sapo()
-        ra = Ra()
-        cobra = Cobra(50,30,100,100,2,2,'terrestre')
-
-        jacare = Jacare(70,40,500,60,10,3,'aquatico')
-        
-        aquatico = Aquatico()
-
-        self.__lista_terreno_aquatico.add(aquatico)
-        self.__lista_player.add(self.__jogador)
-        self.__lista_parceiro.add(ra)
-        self.__lista_cobras.add(cobra)
-        self.__lista_jacares.add(jacare)
-        
-        self.__all_sprites.add(aquatico, self.__jogador, ra)
         
         #FIXME: só para o mvp, dps isso dependerá da fase (que ainda não foi implementada)
-        for flor in range(0, 6):
-            #print("aaa")
-            #print(self.__all_sprites)
-            c1.coordenadas()
-            girassol = Girassol(c1.coordenadax,c1.coordenaday)
-            c1.coordenadas()
-            jasmin = Jasmin(c1.coordenadax,c1.coordenaday)
-            
-            self.__lista_flores.add(girassol, jasmin)
-            self.__all_sprites.add(girassol, jasmin)
-        
-        for cons in range(0,4):
-            c1.coordenadas()
-            maca = Maca(c1.coordenadax,c1.coordenaday)
-            c1.coordenadas()
-            espinho = Espinho(c1.coordenadax,c1.coordenaday)
-            c1.coordenadas()
-            cogumelo = Cogumelo(c1.coordenadax,c1.coordenaday)
 
-            self.__lista_consumiveis.add(maca, espinho)
-            self.__lista_cogumelos.add(cogumelo)
-
-            self.__all_sprites.add(maca, espinho,cogumelo)
-        
-        print(self.__all_sprites)
         self.__tela.iniciar()
         rodando = True
-        
+        sprites = self.__mapa.spawn_all()
+        sprites.add(self.__jogador)
+
         while rodando:  
             self.__clock.tick(40)
             self.__tela.colorir()
-            self.__tela.desenhar(self.__all_sprites)
+            self.__tela.desenhar(sprites)
             if self.colisoes() == 'Perdeu!':
                 break
 
-            cobra.movimento(50,2,3,300,550,100,20,250,"Imagens/cobra_direita.png","Imagens/cobra_baixo.png","Imagens/cobra_esquerda.png","Imagens/cobra_cima.png")
-            jacare.movimento(15,4,5,600,500,500,0,80,"Imagens/jacare_direita.png","Imagens/jacare_baixo.png","Imagens/jacare_esquerda.png","Imagens/jacare_cima.png")
-            
-            distancia_cobra = cobra.distancia_ponto(self.__jogador.rect.x,self.__jogador.rect.y,cobra.rect.x,cobra.rect.y)
-            distancia_jacare = jacare.distancia_ponto(self.__jogador.rect.x,self.__jogador.rect.y,jacare.rect.x,jacare.rect.y)
-            cobra.movimento(50,2,3,300,550,100,20,250,"versao_final/Imagens/cobra_direita.png","versao_final/Imagens/cobra_baixo.png","versao_final/Imagens/cobra_esquerda.png","versao_final/Imagens/cobra_cima.png",distancia_cobra,self.__jogador.rect.x,self.__jogador.rect.y)
-            jacare.movimento(15,4,5,600,500,500,0,80,"versao_final/Imagens/jacare_direita.png","versao_final/Imagens/jacare_baixo.png","versao_final/Imagens/jacare_esquerda.png","versao_final/Imagens/jacare_cima.png",distancia_jacare,self.__jogador.rect.x,self.__jogador.rect.y)
-            
+            for cobra in self.__mapa.lista_cobras:
+                distancia_cobra = cobra.distancia_ponto(self.__jogador.rect.x,self.__jogador.rect.y,cobra.rect.x,cobra.rect.y)
+                cobra.movimento(50,2,3,300,550,100,20,250,"Imagens/cobra_direita.png","Imagens/cobra_baixo.png","Imagens/cobra_esquerda.png","Imagens/cobra_cima.png",distancia_cobra,self.__jogador.rect.x,self.__jogador.rect.y)
+
+            for jacare in self.__mapa.lista_jacares:
+                distancia_jacare = jacare.distancia_ponto(self.__jogador.rect.x,self.__jogador.rect.y,jacare.rect.x,jacare.rect.y)
+                jacare.movimento(15,4,5,600,500,500,0,80,"Imagens/jacare_direita.png","Imagens/jacare_baixo.png","Imagens/jacare_esquerda.png","Imagens/jacare_cima.png",distancia_jacare,self.__jogador.rect.x,self.__jogador.rect.y)
 
             for event in self.__tela.ler():
                 if event.type == pygame.QUIT:
@@ -209,58 +166,46 @@ class GameController:
                         return self.restart()
 
     def restart(self):
-        self.__lista_player = pygame.sprite.Group()
+        self.__mapa.reset()
         self.__jogador = None
-
-        self.__lista_parceiro = pygame.sprite.Group()
         self.__colisoes_parceiro = None
-
-        self.__lista_cobras = pygame.sprite.Group()
         self.__colisoes_cobra = None
-
-        self.__lista_jacares = pygame.sprite.Group()
         self.__colisoes_jacare = None
-
-        self.__lista_flores = pygame.sprite.Group()
         self.__colisoes_flores = None
-
-        self.__lista_consumiveis = pygame.sprite.Group()
         self.__colisoes_consumiveis = None
-
-        self.__lista_cogumelos = pygame.sprite.Group()
         self.__colisoes_cogumelos = None
-
-        self.__lista_terreno_aquatico = pygame.sprite.Group()
         self.__colisoes_terreno_aquatico = None
-        
-        self.__all_sprites = pygame.sprite.Group()
-        
         #chave é o objeto flor e o valor é bool p/ saber se seu peso foi ou não descontado da velocidade do sapo
         self.__flores_coletadas = {}
         self.iniciar()
 
     def colisoes(self):
-        self.__colisoes_cobra = pygame.sprite.spritecollide(self.__jogador, self.__lista_cobras, False)
-        self.__colisoes_jacare = pygame.sprite.spritecollide(self.__jogador, self.__lista_jacares, False)
-        #jogador.flores = pygame.sprite.spritecollide(self.__jogador, self.__lista_girassois, True)
-        self.__colisoes_flores = pygame.sprite.spritecollide(self.__jogador, self.__lista_flores, True)
-        self.__colisoes_terreno_aquatico = pygame.sprite.spritecollide(self.__jogador, self.__lista_terreno_aquatico, False)
-        self.__colisoes_parceiro = pygame.sprite.spritecollide(self.__jogador, self.__lista_parceiro, False)
-        self.__colisoes_consumiveis = pygame.sprite.spritecollide(self.__jogador, self.__lista_consumiveis, True)
-        self.__colisoes_cogumelos = pygame.sprite.spritecollide(self.__jogador,self.__lista_cogumelos,True)
+        self.__colisoes_cobra = pygame.sprite.spritecollide(self.__jogador, self.__mapa.lista_cobras, False)
+        self.__colisoes_jacare = pygame.sprite.spritecollide(self.__jogador, self.__mapa.lista_jacares, False)
+        self.__colisoes_flores = pygame.sprite.spritecollide(self.__jogador, self.__mapa.lista_flores, True)
+        self.__colisoes_terreno_aquatico = pygame.sprite.spritecollide(self.__jogador, self.__mapa.lista_terreno_aquatico, False)
+        self.__colisoes_parceiro = pygame.sprite.spritecollide(self.__jogador, self.__mapa.lista_parceiro, False)
+        self.__colisoes_consumiveis = pygame.sprite.spritecollide(self.__jogador, self.__mapa.lista_consumiveis, True)
+        self.__colisoes_cogumelos = pygame.sprite.spritecollide(self.__jogador,self.__mapa.lista_cogumelos,True)
+
         if self.__colisoes_cobra:
             return("Perdeu!")
+
         elif self.__colisoes_jacare:
             return("Perdeu!")
-        elif self.__colisoes_flores:
+
+        if self.__colisoes_flores:
             flor = self.__colisoes_flores[0]
             self.__flores_coletadas[flor] = self.__jogador.carry(flor.peso)
+
         elif self.__colisoes_consumiveis:
             if self.__jogador.velocidade<0:
                 self.__jogador.debuff()
             self.__jogador.aumenta_velocidade(3)
+
         elif self.__colisoes_cogumelos:
             self.__jogador.debuff()
+
         elif self.__colisoes_parceiro:
             remove = []
             for flor in self.__flores_coletadas:
---
+++ versao_final/mapa.py
@@ -0,0 +1,119 @@
+import pygame
+
+from itens.girassol import Girassol
+from itens.jasmin import Jasmin
+from itens.maca import Maca
+from itens.espinho import Espinho
+from itens.cogumelo import Cogumelo
+
+from terreno.aquatico import Aquatico
+from ra import Ra
+
+from inimigos.cobra import Cobra
+from inimigos.jacare import Jacare
+
+from coordenada import Coordenada
+
+class Mapa:
+    def __init__(self):
+        self.__lista_parceiro = pygame.sprite.Group()
+        self.__lista_cobras = pygame.sprite.Group()
+        self.__lista_jacares = pygame.sprite.Group()
+        self.__lista_flores = pygame.sprite.Group()
+        self.__lista_consumiveis = pygame.sprite.Group()
+        self.__lista_cogumelos = pygame.sprite.Group()
+        self.__lista_terreno_aquatico = pygame.sprite.Group()
+        self.__all_sprites = pygame.sprite.Group()
+        self.__c1 = Coordenada(0,0,0)
+
+    def spawn_flores(self, quantidade):
+        for i in range (quantidade):
+            self.__c1.coordenadas()
+            girassol = Girassol(self.__c1.coordenadax,self.__c1.coordenaday)
+            self.__c1.coordenadas()
+            jasmin = Jasmin(self.__c1.coordenadax,self.__c1.coordenaday)
+            self.__lista_flores.add(girassol, jasmin)
+            self.__all_sprites.add(girassol, jasmin)
+    
+    def spawn_cobras(self):
+        cobra = Cobra(50,30,100,100,2,2,'terrestre')
+        self.__lista_cobras.add(cobra)
+
+
+    def spawn_jacares(self):
+        jacare = Jacare(70,40,500,60,10,3,'aquatico')
+        self.__lista_jacares.add(jacare)
+
+    def spawn_consumiveis(self):
+        for i in range (5):
+            self.__c1.coordenadas()
+            maca = Maca(self.__c1.coordenadax,self.__c1.coordenaday)
+            self.__c1.coordenadas()
+            espinho = Espinho(self.__c1.coordenadax,self.__c1.coordenaday)
+            self.__c1.coordenadas()
+            cogumelo = Cogumelo(self.__c1.coordenadax,self.__c1.coordenaday)
+
+            self.__lista_consumiveis.add(maca, espinho)
+            self.__lista_cogumelos.add(cogumelo)
+            self.__all_sprites.add(maca, espinho,cogumelo)
+
+    def spawn_ra(self):
+        ra = Ra()
+        self.__lista_parceiro.add(ra)
+        self.__all_sprites.add(ra)
+
+    def spawn_terrenos(self):
+        aquatico = Aquatico()
+        self.__lista_terreno_aquatico.add(aquatico)
+        self.__all_sprites.add(aquatico)
+    
+    def spawn_all(self):
+        self.spawn_terrenos()
+        self.spawn_ra()
+        self.spawn_consumiveis()
+        self.spawn_jacares()
+        self.spawn_cobras()
+        self.spawn_flores(5)
+        return self.__all_sprites
+
+    def reset(self):
+        self.__lista_parceiro = pygame.sprite.Group()
+        self.__lista_cobras = pygame.sprite.Group()
+        self.__lista_jacares = pygame.sprite.Group()
+        self.__lista_flores = pygame.sprite.Group()
+        self.__lista_consumiveis = pygame.sprite.Group()
+        self.__lista_cogumelos = pygame.sprite.Group()
+        self.__lista_terreno_aquatico = pygame.sprite.Group()
+        self.__all_sprites = pygame.sprite.Group()
+
+    @property
+    def lista_cobras(self):
+        return self.__lista_cobras
+
+    @property
+    def lista_jacares(self):
+        return self.__lista_jacares
+
+    @property
+    def lista_parceiro(self):
+        return self.__lista_parceiro
+
+    @property
+    def lista_flores(self):
+        return self.__lista_flores
+
+    @property
+    def lista_consumiveis(self):
+        return self.__lista_consumiveis
+
+    @property
+    def lista_cogumelos(self):
+        return self.__lista_cogumelos
+
+    @property
+    def lista_terreno_aquatico(self):
+        return self.__lista_terreno_aquatico
+
+    @property
+    def all_sprites(self):
+        return self.__all_sprites
