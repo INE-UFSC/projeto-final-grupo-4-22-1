8838483727d024e0f6b668f6bad2b04e38a375a8
Fernanda Müller <nandinhak998@gmail.com>
2022-07-09 18:11:31 -0300

    Corrigindo erro de código.

--- versao_final/ra.py
+++ versao_final/ra.py
@@ -4,5 +4,5 @@ import pygame
 ROXO = (72, 61, 139)
 
 class Ra(Personagem):
-    def __init__(self):
-        super().__init__(altura_ra:int, largura_ra:int, coordenadax_ra:int, coordenaday_ra:int, ROXO)
+    def __init__(self, altura_ra:int, largura_ra:int, coordenadax_ra:int, coordenaday_ra:int):
+        super().__init__(altura_ra, largura_ra, coordenadax_ra, coordenaday_ra, ROXO)
