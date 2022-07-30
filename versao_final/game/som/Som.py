754823ab78b2961c2cd2af4ed0a53d2246d68f56
Pedro <pedro.nackm@gmail.com>
2022-07-29 23:27:36 -0300

    volume consertado

--- versao_final/game/som/Som.py
+++ versao_final/game/som/Som.py
@@ -4,14 +4,14 @@ import os
 
 class Som():
     def __init__(self):
+        mixer.init()
+        pygame.mixer.music.set_volume(0.05)
         self.__SomFundo = os.path.join("game/som/musica/musica_fundo.mp3")
         self.__SomInicial = os.path.join("game/som/musica/musica_inicial.wav")
         self.__SomPerdeu = os.path.join("game/som/musica/musica_final_perdeu.ogg")
         self.__SomGanhou = os.path.join("game/som/musica/musica_final_ganhou.mp3")
 
     def iniciar(self,musica):
-        mixer.init()
-        pygame.mixer.music.set_volume(0.05)
         if(musica==0):
             mixer.music.load(self.__SomFundo)
         elif(musica==1):
@@ -23,23 +23,7 @@ class Som():
         mixer.music.play(-1)
 
     def unpause(self):
-        mixer.music.unpause()
-    
+        pygame.mixer.music.set_volume(0.05)
+
     def pause(self):
-        mixer.music.pause()
-    
-    def elemento(self):
-        mixer.init()
-        mixer.music.load(self.__Somsunflower)
-        mixer.music.play(1)
-    """caso vcs queiram testar o código de música basta colocar 
-    pip install pygame==1.9.6
-    no cmd e reiniciar a maquina
-    ai basta descomentar o código do controller e testar"""
-
-
-
-
-    
-    
-    
+        pygame.mixer.music.set_volume(0)
